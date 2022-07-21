3d7464384eb62ee47ddbf8892c8cbadcf6c6a9b4
HusseinYoussef <husseinyoussef@getopenwater.com>
2022-07-22 01:34:55 +0200

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :applications, param: :token, only: [:index, :show, :create, :update] do
+        resources :chats, param: :number, only: [:index, :show, :create, :update] do
+          resources :messages, param: :number, only: [:index, :show, :create, :update]
+        end
+        
+        # Search endpoint
+        post '/chats/:number/search' => 'chats#search'
+      end
+    end
+  end
 end

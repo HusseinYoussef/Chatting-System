8780bb5b2a4ec37b257b989cc1fc783f1f814e53
HusseinYoussef <husseinyoussef@getopenwater.com>
2022-07-22 00:32:47 +0200

    Added validations to Chat model

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,7 +2,10 @@ class Chat < ApplicationRecord
   # Associations
   belongs_to :application
   has_many :messages, dependent: :destroy
-
+  
+  # Validations
+  validates :number, presence: true
+  
   attribute :number, :integer
   attribute :messages_count, :integer, default: 0
 end

4d04ae4816c914a7b75aeb450889b20331bdb418
HusseinYoussef <husseinyoussef@getopenwater.com>
2022-07-22 15:07:06 +0200

    Update callbacks for Chat model

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,7 @@
 class Chat < ApplicationRecord
+  after_commit :create_chat_msg_counter, on: :create
+  after_commit :delete_chat_msg_counter, on: :destroy
+
   # Associations
   belongs_to :application
   has_many :messages, dependent: :destroy
@@ -8,4 +11,18 @@ class Chat < ApplicationRecord
   
   attribute :number, :integer
   attribute :messages_count, :integer, default: 0
+  
+  private
+
+  def chat_msg_counter_key
+    "#{self.application.token}_#{self.number}"
+  end
+
+  def create_chat_msg_counter
+    $redis.set(chat_msg_counter_key, 0)
+  end
+  
+  def delete_chat_msg_counter
+    $redis.del(chat_msg_counter_key)
+  end
 end

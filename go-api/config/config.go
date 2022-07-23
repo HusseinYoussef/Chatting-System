d6c817c7a2119842aa00c132333723ec8e0a9e39
HusseinYoussef <husseinyoussef@getopenwater.com>
2022-07-24 01:38:25 +0200

    Added const configs

---
+++ go-api/config/config.go
@@ -0,0 +1,11 @@
+package config
+
+const (
+	RedisUrl = "localhost:6379"
+	RedisDB  = 0
+
+	ServerName = "localhost:3306"
+	User       = "root"
+	Password   = "password"
+	DB         = "chat-api_development"
+)

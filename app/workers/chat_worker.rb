3b11579a5e913eefbea283c4497620f6b2e8fcf8
HusseinYoussef <husseinyoussef@getopenwater.com>
2022-07-25 17:21:42 +0200

    Added Exception handlers in workers

--- app/workers/chat_worker.rb
+++ app/workers/chat_worker.rb
@@ -1,12 +1,13 @@
 class ChatWorker
     include Sidekiq::Worker
 
-    def perform(app_id, app_token, chat_number)
-        chat = Chat.new(application_id: app_id, application_token: app_token, number: chat_number)
-        if chat.save
-            puts("****** Chat created successfully! ******")
-        else
-            raise "Can't create chat"
+    def perform(app_token, chat_number)
+        chat = Chat.new(application_token: app_token, number: chat_number)
+        begin
+            chat.save!
+            puts("****** Chat number #{chat_number} in Application #{app_token} created successfully! ******")
+        rescue => exception
+            puts("Can't create chat: #{exception.message}")
         end
     end
 end
--- app/workers/message_worker.rb
+++ app/workers/message_worker.rb
@@ -3,10 +3,11 @@ class MessageWorker
 
     def perform(chat_id, msg_number, msg_body)
         message = Message.new(chat_id: chat_id, number: msg_number, body: msg_body)
-        if message.save
-            puts("****** Message created successfully! ******")
-        else
-            raise "Can't create message"
+        begin
+            message.save!
+            puts("****** Message number #{msg_number} in chat_id #{chat_id} created successfully! ******")
+        rescue => exception
+            puts("Can't create message: #{exception.message}")
         end
     end
 end

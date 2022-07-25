d305c81e0ad9f3eccff0644e2afe018d5c49d810
HusseinYoussef <husseinyoussef@getopenwater.com>
2022-07-25 10:54:27 +0200

    Added application_token while creating chats

--- app/workers/chat_worker.rb
+++ app/workers/chat_worker.rb
@@ -1,8 +1,8 @@
 class ChatWorker
     include Sidekiq::Worker
 
-    def perform(app_id, chat_number)
-        chat = Chat.new(application_id: app_id, number: chat_number)
+    def perform(app_id, app_token, chat_number)
+        chat = Chat.new(application_id: app_id, application_token: app_token, number: chat_number)
         if chat.save
             puts("****** Chat created successfully! ******")
         else

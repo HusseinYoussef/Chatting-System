e17988cb21f98fc224c94378d8af19fb66462c0d
HusseinYoussef <husseinyoussef@getopenwater.com>
2022-07-25 17:20:23 +0200

    Configured sidekiq cron

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,8 @@
+schedule_file = "config/schedule.yml"
+if File.exist?(schedule_file) && Sidekiq.server?
+   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+end
+
 redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/0')
 
 Sidekiq.configure_server do |config|

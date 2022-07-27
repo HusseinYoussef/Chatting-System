490207628327cb7a7998cb63034e213d2dab2586
HusseinYoussef <sonamosa21@gmail.com>
2022-07-27 02:42:30 +0200

    Updated Go configs to use Env variables

--- go-api/config/config.go
+++ go-api/config/config.go
@@ -1,12 +1,22 @@
 package config
 
-const (
-	RedisUrl = "localhost:6379"
-	RedisDB  = 0
+import "os"
 
-	ElasticSearchUrl = "http://localhost:9200"
-	HOST             = "localhost:3306"
-	User             = "root"
+func GetEnv(key string, default_value string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists{
+		return default_value
+	}
+	return value
+}
+
+var HOST = GetEnv("HOST", "localhost:3306")
+var RedisUrl = GetEnv("RedisUrl", "localhost:6379") 
+var ElasticSearchUrl = GetEnv("ElasticSearchUrl", "http://localhost:9200")
+
+const (
+	User     		 = "goapp"
 	Password         = "password"
 	DB               = "chat-api_development"
+	RedisDB  		= 0
 )
